28efa01320b95e4659cd2f89c6c970b7f35e4512
Shawn <shawn.mcentyre@gmail.com>
2026-07-08 15:41:10 -0700

    mkvquickplay 2.4.1

--- Casks/mkvquickplay.rb
+++ Casks/mkvquickplay.rb
@@ -1,6 +1,6 @@
 cask "mkvquickplay" do
-  version "2.4.0"
-  sha256 "b4f346930f264fe3a818a1ccba1813e06f2979d23ea95dce0264dfce8b3505f9"
+  version "2.4.1"
+  sha256 "12e50467c962e1ff90d1a11b4bf2e3e267b1b97940982af5430c5210b734bd14"
 
   url "https://github.com/tehshawn/mkvquickplay/releases/download/v#{version}/MKVQuickPlay-macOS-v#{version}.zip",
       verified: "github.com/tehshawn/mkvquickplay/"
@@ -13,6 +13,7 @@ cask "mkvquickplay" do
     strategy :github_latest
   end
 
+  auto_updates true
   depends_on formula: "mpv"
   depends_on macos: :ventura
 
